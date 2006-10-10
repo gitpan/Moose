ce14efda8e6bfb7bf7c29c0c763a74f9c865a381
Stevan Little <stevan@cpan.org>
2006-10-10 00:03:02 +0000

    Import of STEVAN/Moose-0.14 from CPAN.
    gitpan-cpan-distribution: Moose
    gitpan-cpan-version:      0.14
    gitpan-cpan-path:         STEVAN/Moose-0.14.tar.gz
    gitpan-cpan-author:       STEVAN
    gitpan-cpan-maturity:     released

--- lib/Moose/Meta/Attribute.pm
+++ lib/Moose/Meta/Attribute.pm
@@ -7,7 +7,7 @@ use warnings;
 use Scalar::Util 'blessed', 'weaken', 'reftype';
 use Carp         'confess';
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 use Moose::Util::TypeConstraints ();
 
@@ -165,9 +165,7 @@ sub _process_options {
 	
 	if (exists $options->{coerce} && $options->{coerce}) {
 	    (exists $options->{type_constraint})
-	        || confess "You cannot have coercion without specifying a type constraint";
-	    #(!$options->{type_constraint}->isa('Moose::Meta::TypeConstraint::Union'))
-	    #    || confess "You cannot have coercion with a type constraint union";	        
+	        || confess "You cannot have coercion without specifying a type constraint";	        
         confess "You cannot have a weak reference to a coerced value"
             if $options->{weak_ref};	        
 	}	
@@ -179,15 +177,6 @@ sub _process_options {
          $options->{type_constraint}->is_a_type_of('HashRef'))
 	        || confess "You cannot auto-dereference anything other than a ArrayRef or HashRef";	        
 	}
-	
-    if (exists $options->{type_constraint} && 
-               ($options->{type_constraint}->is_a_type_of('ArrayRef') ||
-                $options->{type_constraint}->is_a_type_of('HashRef')  )) { 
-        unless (exists $options->{default}) {
-            $options->{default} = sub { [] } if $options->{type_constraint}->name eq 'ArrayRef';
-            $options->{default} = sub { {} } if $options->{type_constraint}->name eq 'HashRef';            
-        }
-    }
 	
 	if (exists $options->{lazy} && $options->{lazy}) {
 	    (exists $options->{default})
@@ -267,6 +256,24 @@ sub _inline_check_required {
 sub _inline_check_lazy {
     my $self = shift;
 	return '' unless $self->is_lazy;
+	if ($self->has_type_constraint) {
+	    # NOTE:
+	    # this could probably be cleaned 
+	    # up and streamlined a little more
+	    return 'unless (exists $_[0]->{$attr_name}) {' .
+	           '    if ($attr->has_default) {' .
+	           '        my $default = $attr->default($_[0]);' .
+               '        (defined($attr->type_constraint->check($default)))' .
+               '        	|| confess "Attribute (" . $attr->name . ") does not pass the type constraint ("' .
+               '               . $attr->type_constraint->name . ") with " . (defined($default) ? "\'$default\'" : "undef")' .
+               '          if defined($default);' .	                
+	           '        $_[0]->{$attr_name} = $default; ' .
+	           '    }' .
+	           '    else {' .
+               '        $_[0]->{$attr_name} = undef;' .
+	           '    }' .
+	           '}';	    
+	}
     return '$_[0]->{$attr_name} = ($attr->has_default ? $attr->default($_[0]) : undef)'
          . 'unless exists $_[0]->{$attr_name};';
 }
--- t/071_misc_attribute_tests.t
+++ t/071_misc_attribute_tests.t
@@ -3,35 +3,43 @@
 use strict;
 use warnings;
 
-use Test::More tests => 7;
+use Test::More tests => 4;
 use Test::Exception;
 
 BEGIN {
     use_ok('Moose');           
 }
 
-## Roles
-
 {
-    package Test::TheDefaultFor::ArrayRef::and::HashRef;
-    use Moose;
+    {
+        package Test::For::Lazy::TypeConstraint;
+        use Moose;
+        use Moose::Util::TypeConstraints;
+
+        has 'bad_lazy_attr' => (
+            is => 'rw',
+            isa => 'ArrayRef',
+            lazy => 1, 
+            default => sub { "test" },
+        );
+        
+        has 'good_lazy_attr' => (
+            is => 'rw',
+            isa => 'ArrayRef',
+            lazy => 1, 
+            default => sub { [] },
+        );        
+
+    }
+
+    my $test = Test::For::Lazy::TypeConstraint->new;
+    isa_ok($test, 'Test::For::Lazy::TypeConstraint');
     
-    has 'array_ref' => (is => 'rw', isa => 'ArrayRef');
-    has 'hash_ref'  => (is => 'rw', isa => 'HashRef');    
-
+    dies_ok {
+        $test->bad_lazy_attr;
+    } '... this does not work';
+    
+    lives_ok {
+        $test->good_lazy_attr;
+    } '... this does not work';    
 }
-
-my $test = Test::TheDefaultFor::ArrayRef::and::HashRef->new;
-isa_ok($test, 'Test::TheDefaultFor::ArrayRef::and::HashRef');
-
-is_deeply($test->array_ref, [], '.... got the right default value');
-is_deeply($test->hash_ref,  {}, '.... got the right default value');
-
-my $test2 = Test::TheDefaultFor::ArrayRef::and::HashRef->new(
-    array_ref => [ 1, 2, [] ],
-    hash_ref  => { one => 1, two => 2, three => {} },
-);
-isa_ok($test2, 'Test::TheDefaultFor::ArrayRef::and::HashRef');
-
-is_deeply($test2->array_ref, [ 1, 2, [] ], '.... got the right default value');
-is_deeply($test2->hash_ref,  { one => 1, two => 2, three => {} }, '.... got the right default value');
