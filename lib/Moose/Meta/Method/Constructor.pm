763a8de6261115e35874d95fc548b265748366d6
Dave Rolsky <autarch@urth.org>
2009-02-24 04:48:45 +0000

    Import of DROLSKY/Moose-0.72 from CPAN.
    gitpan-cpan-distribution: Moose
    gitpan-cpan-version:      0.72
    gitpan-cpan-path:         DROLSKY/Moose-0.72.tar.gz
    gitpan-cpan-author:       DROLSKY
    gitpan-cpan-maturity:     released

--- lib/Moose/Meta/Method/Constructor.pm
+++ lib/Moose/Meta/Method/Constructor.pm
@@ -6,7 +6,7 @@ use warnings;
 
 use Scalar::Util 'blessed', 'weaken', 'looks_like_number';
 
-our $VERSION   = '0.71_01';
+our $VERSION   = '0.72';
 our $AUTHORITY = 'cpan:STEVAN';
 
 use base 'Moose::Meta::Method',
@@ -177,7 +177,7 @@ sub initialize_body {
             '@type_constraint_bodies' => \@type_constraint_bodies,
         },
     ) or $self->throw_error("Could not eval the constructor :\n\n$source\n\nbecause :\n\n$@", error => $@, data => $source );
-
+    
     $self->{'body'} = $code;
 }
 
@@ -190,7 +190,7 @@ sub _generate_BUILDARGS {
         return join("\n",
             'do {',
             $self->_inline_throw_error('"Single parameters to new() must be a HASH ref"', 'data => $_[0]'),
-            '    if scalar @_ == 1 && defined $_[0] && ref($_[0]) ne q{HASH};',
+            '    if scalar @_ == 1 && !( defined $_[0] && ref $_[0] eq q{HASH} );',
             '(scalar @_ == 1) ? {%{$_[0]}} : {@_};',
             '}',
         );
--- lib/Moose/Object.pm
+++ lib/Moose/Object.pm
@@ -7,7 +7,7 @@ use warnings;
 use if ( not our $__mx_is_compiled ), 'Moose::Meta::Class';
 use if ( not our $__mx_is_compiled ), metaclass => 'Moose::Meta::Class';
 
-our $VERSION   = '0.71_01';
+our $VERSION   = '0.72';
 $VERSION = eval $VERSION;
 our $AUTHORITY = 'cpan:STEVAN';
 
@@ -21,16 +21,14 @@ sub new {
 
 sub BUILDARGS {
     my $class = shift;
-    if (scalar @_ == 1) {
-        if (defined $_[0]) {
-            (ref($_[0]) eq 'HASH')
-                || $class->meta->throw_error("Single parameters to new() must be a HASH ref", data => $_[0]);
-            return {%{$_[0]}};
-        } 
-        else {
-            return {}; # FIXME this is compat behavior, but is it correct?
+    if ( scalar @_ == 1 ) {
+        unless ( defined $_[0] && ref $_[0] eq 'HASH' ) {
+            $class->meta->throw_error(
+                "Single parameters to new() must be a HASH ref",
+                data => $_[0] );
         }
-    } 
+        return { %{ $_[0] } };
+    }
     else {
         return {@_};
     }
